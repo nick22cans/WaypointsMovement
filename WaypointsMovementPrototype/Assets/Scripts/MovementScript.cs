3b547be3ab4279b82237801929779ff140912288
Nick Veselov <nick.veselov.gamedev@gmail.com>
2017-04-20 13:12:37 +0100

    Prelunch commit

--- WaypointsMovementPrototype/Assets/Scripts/MovementScript.cs
+++ WaypointsMovementPrototype/Assets/Scripts/MovementScript.cs
@@ -7,32 +7,34 @@ public class MovementScript : MonoBehaviour {
 	//rough amount of movement per frame
 	public float speed;
 	//distance calculation error amplitude
-	public float distance_epsilon;
-	//amount of lookup range required to switch to the next wp 
-	public float waypointSwitchModifier;
+	public float distance_sqrEpsilon;
+	//direction difference error while turning
+	public float direction_sqrEpsilon;
 
 	public float lookAheadDistance;
 
-	#region waypoints
+
 	private int m_currentWaypointIndex;
-	//private float m_sqrCurrentWpReachingDistance;
 	private Vector3 m_currentWaypoint;
 	private bool m_waypointIsStrict;
-	#endregion
 
 	private bool m_isMovingAlongTheRoute = false;
+	private bool m_isTurning = false;
+	private bool m_switchingToTheNextWaypoint = false;
+	private bool m_thereIsAtLeastOneMoreWaypoint = false;
 
-	#region direction interpolation
-	private bool m_isInterpolatingDirection;
-	private Vector3 m_interpolationStep;
-	//not really needed now -> prototype for dynamic update
-	private Vector3 m_fromDirection;
-	private Vector3 m_toDirection;
-	private Vector3 m_currentDirection;
+	private Vector3 m_intersectionPoint;
+
+	private Vector3 m_direction;
 	private Vector3 m_lookAheadPoint;
-	#endregion
 
-	private float previousFrameDistance;
+	private float m_previousFrameDistance;
+	private float m_currentFrameDistance;
+
+	float m_rotationStepsNumber = 10;
+	Vector3 m_directionChangeStep; 
+
+	//private float previousFrameDistance;
 
 	// Use this for initialization
 	void Start () {
@@ -41,106 +43,188 @@ public class MovementScript : MonoBehaviour {
 
 	public void SetUpRoute()
 	{
-		if (waypoints.Length > 0) {
+		if (waypoints.Length > 0)
+		{
 			m_currentWaypointIndex = -1;
-			if (PickNewWaypoint()) {
+			if (SwitchToTheNextWaypoint ())
+			{
 				m_isMovingAlongTheRoute = true;
 			}
 		}
 	}
 
-	bool PickNewWaypoint()
+	bool SwitchToTheNextWaypoint()
 	{
 		m_currentWaypointIndex++;
 		if (m_currentWaypointIndex == waypoints.Length)
 			return false;
-		
+		if (m_currentWaypointIndex != waypoints.Length - 1)
+			m_thereIsAtLeastOneMoreWaypoint = true;
+		else
+			m_thereIsAtLeastOneMoreWaypoint = false;
+
 		m_currentWaypoint = waypoints [m_currentWaypointIndex].transform.position;
 		m_waypointIsStrict = (waypoints [m_currentWaypointIndex]).GetComponent<WaypointScript> ().isStrict;
-		previousFrameDistance = GetDistanceToThePoint (m_currentWaypoint);
-		m_lookAheadPoint = m_currentWaypoint + m_currentDirection* lookAheadDistance;
-		//m_currentDirection = GetDirectionTowardsWaypoint ();
+
+//		if (!m_isTurning)
+			m_direction = GetDirectionTowardsWaypoint ();
+		m_isTurning = false;
 		return true;
 	}
 
-	bool IsWaypointReached()
+	void UpdateLookAheadPoint()
 	{
-		
-		float distance = (m_lookAheadPoint - transform.position).magnitude - Mathf.Max(transform.localScale.x, transform.localScale.z);
-		print (distance);
-		if (distance < lookAheadDistance*waypointSwitchModifier && !m_waypointIsStrict)
+
+
+		if (m_isTurning && m_thereIsAtLeastOneMoreWaypoint)
+		{
+			if (GetTwoLinesIntersection (transform.position, PredictPointInCurrentDirection(lookAheadDistance), m_currentWaypoint, waypoints [m_currentWaypointIndex + 1].transform.position))
+			{
+				float distanceToTheIntersection = GetDistanceToThePoint (m_intersectionPoint);
+				Vector3 crossWpDirection = GetDirectionFromOnePointToAnother (m_currentWaypoint, waypoints [m_currentWaypointIndex + 1].transform.position);
+				float turnAmplitude = lookAheadDistance - distanceToTheIntersection;
+
+				m_currentFrameDistance = turnAmplitude;
+				print (turnAmplitude);
+//				if (turnAmplitude < 1)
+//					speed = speed;
+
+				m_lookAheadPoint = m_currentWaypoint + crossWpDirection * turnAmplitude;
+			}
+		}
+		else
+			m_lookAheadPoint = PredictPointInCurrentDirection (lookAheadDistance);
+	}
+
+	bool LookAheadPointIsCloseToTheWaypoints()
+	{
+		return (GetSqrDistanceBetweenPoints (m_currentWaypoint, m_lookAheadPoint) < distance_sqrEpsilon);
+	}
+
+	bool CurrentWaypointIsBehind()
+	{
+//		if (m_isTurning && m_thereIsAtLeastOneMoreWaypoint)
+//		{
+//			if (GetSqrDistanceBetweenPoints (
+//				GetDirectionFromOnePointToAnother(transform.position,waypoints[m_currentWaypointIndex+1].transform.position),
+//					m_direction) < direction_sqrEpsilon)
+//			{
+//				m_isTurning = false;
+//				return true;
+//			}			
+//		}
+
+		if (m_currentFrameDistance < m_previousFrameDistance)
+		{
+			if (m_thereIsAtLeastOneMoreWaypoint)
+			{
+				m_switchingToTheNextWaypoint = true;
+				m_directionChangeStep = (GetDirectionTowardsPoint (waypoints [m_currentWaypointIndex + 1].transform.position) - m_direction) / m_rotationStepsNumber;
+			}
 			return true;
-		if (m_waypointIsStrict && GetDistanceToThePoint (m_currentWaypoint) < distance_epsilon)
-			return true;
+		}
+		m_previousFrameDistance = m_currentFrameDistance;
 		return false;
 	}
 
-	float GetDistanceToALine(Vector3 lineA)
+	bool GetTwoLinesIntersection(Vector3 l1_s, Vector3 l1_e, Vector3 l2_s, Vector3 l2_e)
 	{
-		Vector3 lineB = transform.position - m_currentWaypoint;
-		return(Mathf.Sin (Vector3.Angle (lineA, lineB)) * lineB.magnitude);
-	}
+		float a1 = l1_e.z - l1_s.z,
+		b1 = l1_s.x - l1_e.x,
+		c1 = a1 * l1_s.x + b1 * l1_s.z;
 
+		float a2 = l2_e.z - l2_s.z,
+		b2 = l2_s.x - l2_e.x,
+		c2 = a2 * l2_s.x + b2 * l2_s.z;
+
+		float delta = a1 * b2 - a2 * b1;
+
+		if (delta == 0)
+			return false;
+		m_intersectionPoint = new Vector3 ((b2 * c1 - b1 * c2) / delta, (l1_e.y - l1_s.y)/2, (a1 * b2 - a2 * c1) / delta);
+		return true;
+	}
+		
 	void PerformStep()
 	{
-
-		//m_lookAheadPoint = transform.position + m_currentDirection * lookAheadDistance;
-		float distanceToWp = GetDistanceToThePoint (m_currentWaypoint);
-			//lookDistance = GetDistanceToThePoint (m_lookAheadPoint);
-
-
-		if (distanceToWp < lookAheadDistance) {
-			if (!m_waypointIsStrict){
-				if (m_currentWaypointIndex != waypoints.Length - 1) {
-
-					Vector3 crossWpDirection = GetDirectionFromOnePointToAnother (m_currentWaypoint, waypoints [m_currentWaypointIndex + 1].transform.position);
-					//float distanceToAnEdge = Mathf.Abs (GetDistanceToALine (crossWpDirection));
-					//print (lookAheadDistance - distanceToAnEdge);
-					m_currentDirection = GetDirectionFromOnePointToAnother (transform.position,
-						m_currentWaypoint + crossWpDirection * (lookAheadDistance - distanceToWp));
-					m_lookAheadPoint = m_currentWaypoint + crossWpDirection * (lookAheadDistance - distanceToWp);
-//
-//					m_currentDirection = GetDirectionFromOnePointToAnother (transform.position,
-//						m_currentWaypoint + crossWpDirection * (lookAheadDistance - distanceToWp));
-//					m_lookAheadPoint = m_currentWaypoint + crossWpDirection * (lookAheadDistance - distanceToWp);
-				}
-			}
-		} else {
-			m_currentDirection = GetDirectionTowardsWaypoint ();
-			m_lookAheadPoint = transform.position + lookAheadDistance * m_currentDirection;
+		if (m_switchingToTheNextWaypoint = true)
+		{
+			if (GetSqrDistanceBetweenPoints (m_direction, GetDirectionTowardsWaypoint ()) < direction_sqrEpsilon)
+				m_switchingToTheNextWaypoint = false;
+			else
+				m_direction += m_directionChangeStep;
 		}
-		transform.position += m_currentDirection * speed;
-		previousFrameDistance = distanceToWp;
+	
+
+		UpdateLookAheadPoint ();
+		m_direction = GetDirectionTowardsPoint (m_lookAheadPoint);
+		transform.position += m_direction * speed;
+
+		if (!m_isTurning)
+			if (GetDistanceToThePoint (m_currentWaypoint) < lookAheadDistance)
+			{
+				m_isTurning = true;
+				m_previousFrameDistance = 0;
+			}
+	}
+
+	float GetDistanceBetweenPoints(Vector3 p1, Vector3 p2){
+		return (p2 - p1).magnitude;
+	}
+
+	float GetSqrDistanceBetweenPoints(Vector3 p1, Vector3 p2){
+		return (p2 - p1).sqrMagnitude;
 	}
 
 	float GetDistanceToThePoint(Vector3 point) {
 		return ((point - transform.position).magnitude);
 	}
 
-	Vector3 GetDirectionTowardsWaypoint(){
-		return (m_currentWaypoint - transform.position).normalized;
-	}
-
 	Vector3 GetDirectionFromOnePointToAnother(Vector3 from, Vector3 to)
 	{
-		return (to - from).normalized;
+		Vector3 result = (to - from).normalized;
+		result.y = 0;	
+		return result;
+	}
+
+	Vector3 GetDirectionTowardsPoint(Vector3 point){
+		return GetDirectionFromOnePointToAnother(transform.position, point);
+	}
+
+	Vector3 GetDirectionTowardsWaypoint(){
+		return GetDirectionTowardsPoint(m_currentWaypoint);
+	}
+
+	Vector3 PredictPointInCurrentDirection(float distance){
+		return(transform.position + m_direction * distance);
 	}
 
 
 	// Update is called once per frame
-	void Update () {
-		if (m_isMovingAlongTheRoute) {
-			if (IsWaypointReached ()) {
-				if (!PickNewWaypoint ())
-					m_isMovingAlongTheRoute = false;
-			} else
-				PerformStep ();
+	void Update () 
+	{
+		if (m_isMovingAlongTheRoute) 
+		{
+			PerformStep ();
+			if (m_isTurning && m_thereIsAtLeastOneMoreWaypoint) 
+			{
+				if (CurrentWaypointIsBehind ()) 
+				{
+					if (!SwitchToTheNextWaypoint ()) 
+					{
+						m_isMovingAlongTheRoute = false;
+						transform.position = m_currentWaypoint;
+					}
+				}
+			}
 		}
 	}
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.red;
-		Gizmos.DrawSphere (m_lookAheadPoint,1f);
+		Gizmos.DrawSphere (m_lookAheadPoint, 1f);
 		Gizmos.DrawLine (m_lookAheadPoint, transform.position);
+
+		Gizmos.DrawCube (PredictPointInCurrentDirection (lookAheadDistance), Vector3.one);
 	}
 }
